cc47002124beef77ee89424be7be12a2aad271c6
danil <danil@DESKTOP-B5464OT>
2020-06-23 11:25:11 +0500

    Добавьте файлы проекта.

---
+++ ооп6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ооп6
+{
+    class Numbers
+
+    {
+        private int OneKonst;
+        private int TwoKonst;
+
+       
+
+
+        public Numbers(int OneObject)
+
+        {
+            OneKonst = OneObject;
+        }
+
+        public Numbers()
+        {
+        }
+
+
+        public Numbers(int One, int Two)
+        {
+            OneKonst = One;
+
+            TwoKonst = Two;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{OneKonst} {TwoKonst}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers Numbers = new Numbers();
+
+            Numbers.Display();
+
+            Numbers NumbersOne = new Numbers(1);
+
+            NumbersOne.Display();
+
+            Numbers NumbersTwo = new Numbers(2, 3);
+
+            NumbersTwo.Display();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
